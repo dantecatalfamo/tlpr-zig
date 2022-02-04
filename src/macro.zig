031297f17fa71ab78b8a801f125c1feeb4326782
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-02-04 13:44:49 -0500

    Fix word wrap

--- src/macro.zig
+++ src/macro.zig
@@ -296,16 +296,26 @@ pub fn wordWrap(line: []u8, width: u8) void {
 
     for (line) |char, idx| {
         const cur_line = idx - last_newline;
-        std.debug.print("thing: {d}\n", .{ cur_line });
         if (std.ascii.isSpace(char)) {
             last_space = idx;
         }
-        if (cur_line == width-1) {
-            std.debug.print("newline added\n", .{});
+        if (char == '\n') {
+            last_newline = idx;
+        }
+        if (cur_line == width-2) {
             last_newline = idx;
             line[last_space] = '\n';
         }
     }
+}
+
+test "word wrap" {
+    var allocator = std.testing.allocator;
+    const text = "hello this is a very long bunch of text I think this should serve as a good test, hopefully I'm able to catch some bugs using this since\nit's very long also hello\nthis is a nice day don't you think? Very cool and good, goodbye!";
+    var txt = try allocator.dupe(u8, text);
+    defer allocator.free(txt);
+    wordWrap(txt, 30);
+    std.debug.print("{s}\n", .{txt});
 }
 
 const macroKeywords = enum {
